750fca9cbf3f87cf3d3fab7c5ceb548c6cf8f108
chananelaze <azenkot.chananel@gmail.com>
2024-03-10 21:59:30 +0200

    added

--- frontend/src/CardsPages/MyOperation.jsx
+++ frontend/src/CardsPages/MyOperation.jsx
@@ -10,7 +10,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import EditIcon from "@mui/icons-material/Edit";
 import IconButton from "@mui/material/IconButton";
-import moment from 'moment';
+import moment from "moment";
 import Button from "@mui/material/Button";
 import "../styles/operation.css";
 import CreateCards from "../CardBusiness/CreateOperation";
@@ -19,14 +19,14 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.black,
     color: theme.palette.common.white,
-    textAlign: 'center',
-    border: '1px solid white',
-    borderRadius: '8px 8px 0 0',
+    textAlign: "center",
+    border: "1px solid white",
+    borderRadius: "8px 8px 0 0",
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
-    textAlign: 'center',
-    border: '1px solid black',
+    textAlign: "center",
+    border: "1px solid black",
   },
 }));
 
@@ -36,30 +36,27 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 
   "&:last-child td, &:last-child th": {
-    border: '1px solid black',
-    textAlign: 'center',
+    border: "1px solid black",
+    textAlign: "center",
   },
 }));
 
 export default function MyOperation() {
   const [operation, setOperation] = useState([]);
-useEffect(() => {
-  fetch(
-    `http://localhost:4000/api/operationId`,
-    {
+  useEffect(() => {
+    fetch(`http://localhost:4000/api/operationId`, {
       credentials: "include",
       method: "GET",
       headers: {
         "Content-type": "application/json",
-        'Authorization': localStorage.token,
-      }
-    }
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      setOperation(data);
-    });
-}, []);
+        Authorization: localStorage.token,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setOperation(data);
+      });
+  }, []);
 
   return (
     <>
@@ -67,7 +64,9 @@ useEffect(() => {
         <TableContainer component={Paper} id="container">
           <div className="btnGroup">
             {<CreateCards />}
-          <Button variant="contained" id="BtnStart">הפסק נתונים</Button>
+            <Button variant="contained" id="BtnStart">
+              הפסק נתונים
+            </Button>
           </div>
           <Table sx={{ minWidth: 700 }} aria-label="customized table">
             <TableHead>
@@ -79,7 +78,9 @@ useEffect(() => {
                 <StyledTableCell align="right">פיריון</StyledTableCell>
                 <StyledTableCell align="right">ניתוק - TV</StyledTableCell>
                 <StyledTableCell align="right">ניתוק - Fiber</StyledTableCell>
-                <StyledTableCell align="right">אחוז שימור - Fiber</StyledTableCell>
+                <StyledTableCell align="right">
+                  אחוז שימור - Fiber
+                </StyledTableCell>
                 <StyledTableCell align="right">אחוז שימור - TV</StyledTableCell>
                 <StyledTableCell align="right">מכר - Fiber</StyledTableCell>
                 <StyledTableCell align="right">מכר - TV</StyledTableCell>
@@ -87,64 +88,65 @@ useEffect(() => {
                 <StyledTableCell align="right">שדרוג</StyledTableCell>
                 <StyledTableCell align="right">סמ׳׳ט</StyledTableCell>
                 <StyledTableCell align="right">יעדים</StyledTableCell>
-                <StyledTableCell align="right">
-                  עדכון פרטים
-                </StyledTableCell>
+                <StyledTableCell align="right">עדכון פרטים</StyledTableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {Array.isArray(operation) && operation.map(operation => (
-                <StyledTableRow key={operation.id}>
-                  <StyledTableCell component="th" scope="row">
-                    {moment(operation.createTime).format('DD/MM/YYYY')}
-                  </StyledTableCell>
-                  <StyledTableCell component="th" scope="row">
-                    {operation.teamName}
-                  </StyledTableCell>
-                  <StyledTableCell component="th" scope="row">
-                    {operation.nameAgent}
-                  </StyledTableCell>
-                  <StyledTableCell align="right">
-                    {operation.numberCalls}
-                  </StyledTableCell>
-                  <StyledTableCell align="right">
-                    {operation.productivity}
-                  </StyledTableCell>
-                  <StyledTableCell align="right">
-                    {operation.tvDisconnection}
-                  </StyledTableCell>
-                  <StyledTableCell align="right">
-                    {operation.fiberDisconnection}
-                  </StyledTableCell>
-                  <StyledTableCell align="right">
-                    {operation.simurFiber}
-                  </StyledTableCell>
-                  <StyledTableCell align="right">
-                    {operation.simurTV}
-                  </StyledTableCell>
-                  <StyledTableCell align="right">
-                    {operation.sellerFiber}
-                  </StyledTableCell>
-                  <StyledTableCell align="right">
-                    {operation.sellerTV}
-                  </StyledTableCell>
-                  <StyledTableCell align="right">
-                    {operation.easyMesh}
-                  </StyledTableCell>
-                  <StyledTableCell align="right">
-                    {operation.upgradeProgress}
-                  </StyledTableCell>
-                  <StyledTableCell align="right">
-                    {operation.satisfaction}
-                  </StyledTableCell>
-                  <StyledTableCell align="right">{operation.targets}</StyledTableCell>
-                  <StyledTableCell align="right">
-                    <IconButton>
-                      <EditIcon />
-                    </IconButton>
-                  </StyledTableCell>
-                </StyledTableRow>
-              ))}
+              {Array.isArray(operation) &&
+                operation.map((operation, index) => (
+                  <StyledTableRow key={operation.id || index}>
+                    <StyledTableCell component="th" scope="row">
+                      {moment(operation.createTime).format("DD/MM/YYYY")}
+                    </StyledTableCell>
+                    <StyledTableCell component="th" scope="row">
+                      {operation.teamName}
+                    </StyledTableCell>
+                    <StyledTableCell component="th" scope="row">
+                      {operation.nameAgent}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {operation.numberCalls}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {operation.productivity}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {operation.tvDisconnection}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {operation.fiberDisconnection}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {operation.simurFiber}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {operation.simurTV}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {operation.sellerFiber}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {operation.sellerTV}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {operation.easyMesh}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {operation.upgradeProgress}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {operation.satisfaction}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {operation.targets}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      <IconButton>
+                        <EditIcon />
+                      </IconButton>
+                    </StyledTableCell>
+                  </StyledTableRow>
+                ))}
             </TableBody>
           </Table>
         </TableContainer>
