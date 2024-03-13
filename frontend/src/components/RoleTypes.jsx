0b7390bb8eb95762ce46f73833b5632c9f79b9dd
chananelaze <azenkot.chananel@gmail.com>
2024-03-13 23:35:50 +0200

    added

--- frontend/src/components/RoleTypes.jsx
+++ frontend/src/components/RoleTypes.jsx
@@ -22,22 +22,22 @@ export const pages = [
   {
     route: "/dailyOperation",
     title: "תפעול יומי",
-    permissions: [RoleTypes.user, RoleTypes.IsBusiness, RoleTypes.isAdmin],
+    permissions: [RoleTypes.user],
   },
   {
     route: "/incrementalOperation",
     title: "תפעול מצטבר",
-    permissions: [RoleTypes.user, RoleTypes.IsBusiness, RoleTypes.isAdmin],
+    permissions: [RoleTypes.user],
   },
   {
     route: "/dailySalesOperation",
     title: "מכירות יומי",
-    permissions: [RoleTypes.user, RoleTypes.IsBusiness, RoleTypes.isAdmin],
+    permissions: [RoleTypes.user],
   },
     {
     route: "/incrementalSalesOperation",
     title: "מכירות מצטבר",
-    permissions: [RoleTypes.user, RoleTypes.IsBusiness, RoleTypes.isAdmin],
+    permissions: [RoleTypes.user],
   },
   {
     route: "/operationTeams",
