74f3dc913b20323da09addfd7802e69d95aeeb8d
chananelaze <azenkot.chananel@gmail.com>
2024-04-21 12:34:08 +0300

    added

--- frontend/src/components/FooterPage.jsx
+++ frontend/src/components/FooterPage.jsx
@@ -4,13 +4,16 @@ import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
 import "../styles/Navbar.css";
 
-export default function FooterPage() {
 
+export default function FooterPage() {
   return (
     <AppBar id="myNavBarFooter" position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              {
+                <h3>the app is developer by Chananel Azenkot tel : 0528308242</h3>
+              }
           </Box>
           <Box
             sx={{
